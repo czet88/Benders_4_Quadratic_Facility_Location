fd6cac8ddc28c6dabbef1cf388682838544debc4
Wach <czet_88@hotmail.com>
2021-01-18 19:38:36 -0500

    Modified time limit

--- src/solve_Benders_model.c
+++ src/solve_Benders_model.c
@@ -46,7 +46,8 @@ int SetBranchandCutParam(CPXENVptr env, CPXLPptr lp) {	//Here we will set the br
 	CPXsetintparam(env, CPX_PARAM_PREIND, 0);
 	CPXsetdblparam(env, CPX_PARAM_CUTUP, UpperBound + 0.001); // provide an initial upper bound
 	CPXsetintparam(env, CPX_PARAM_MIPEMPHASIS, CPX_MIPEMPHASIS_OPTIMALITY);  // MIP emphasis: optimality, feasibility, moving best bound
-	CPXsetdblparam(env, CPX_PARAM_TILIM, 86400); // time limit
+	//CPXsetdblparam(env, CPX_PARAM_TILIM, 86400); // time limit
+	CPXsetdblparam(env, CPX_PARAM_TILIM, 1000); // time limit
 	//CPXsetdblparam(env, CPX_PARAM_TRELIM, 14000); // B&B memory limit
 	CPXsetdblparam(env, CPX_PARAM_EPGAP, 0.0000001); // e-optimal solution (%gap)
 											  //CPXsetdblparam(env, CPX_PARAM_CUTSFACTOR, 1.0); // add cuts or not
