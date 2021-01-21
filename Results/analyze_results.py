cc263467d5ba8501fe0eed24d092d8f9e4bf9870
Wach <czet_88@hotmail.com>
2021-01-21 15:26:17 -0500

    Code now ready to automatically generate the tables we want for each case

--- Results/analyze_results.py
+++ Results/analyze_results.py
@@ -1,11 +1,13 @@
 import pandas as pd
+import numpy as np
+import re
 import sys
 import time
 import os
 from glob import glob
 
 
-def create_latex_table(df, col_list=None):
+def create_latex_table(df, col_list=None, caption_string=None):
     # Function that reads the dataframe and a list of ordered columns
     # to be included and outputs the string to be pasted in a latex document
     if not df.empty:
@@ -13,23 +15,91 @@ def create_latex_table(df, col_list=None):
             cols = col_list
         else:
             cols = df.columns.tolist()
-        out_string = df[cols].to_latex(index=False)
+        # Add caption if there is one
+        if caption_string:
+            out_string = df[cols].to_latex(index=True, index_names=True, multicolumn_format='c', caption=caption_string)
+        else:
+            out_string = df[cols].to_latex(index=True, index_names=True, multicolumn_format='c')
     else:
         out_string = "Empty dataframe"
     return out_string
+
+
+def name_heur(row):
+    # Create a column with the name of the heuristic used
+    if row['heurParam'] == 1:
+        return 'MH'
+    if row['heurParam'] == 2:
+        return 'MH_LS'
+    if row['heurParam'] == 3:
+        return 'MH_LS_ILS'
+    return 'Other'
 
 
 def from_csvs_to_dataframe(file_prefix):
     # Function that reads all files in the current folder beginning with file_prefix
     # and concatenates them all into one dataframe.
     print("Searching for files beginning with " + file_prefix + ".")
-    path_pf = f'./{file_prefix}*.csv'
+    path_pf = f'./{file_prefix}*'
     file_list = glob(path_pf)
     list_of_dfs = []
     for file in file_list:
         list_of_dfs.append(pd.read_csv(file, delimiter=";"))
     full_df = pd.concat(list_of_dfs)
+
+    # Rename some of the columns
+    full_df['Heur_used'] = full_df.apply(lambda row: name_heur(row), axis=1)
+    full_df.rename(columns={'instance': 'Instance'}, inplace=True)
     return full_df
+
+
+def split_dataframe_by_case(df_full):
+    # List of conditional statements
+    uncap_cond = df_full['Cap'] == 0
+    cap_cond = df_full['Cap'] == 1
+    fix_charge_cond = df_full['hybrid'] == 1
+    p_med_cond = df_full['hybrid'] == 0
+
+    # Uncapacitated dataframes
+    df_uncap_fix_charge = df_full[uncap_cond & fix_charge_cond]
+    df_uncap_p_med = df_full[uncap_cond & p_med_cond]
+
+    # Capacitated dataframes
+    df_cap_fix_charge = df_full[cap_cond & fix_charge_cond]
+    df_cap_p_med = df_full[cap_cond & p_med_cond]
+
+    # Create a dictionary for each of the cases
+    df_dict = {"uncap_fixed_cost": [df_uncap_fix_charge, "Results for the uncapacitated fixed charge problems"],
+               "cap_fixed_cost": [df_cap_fix_charge, "Results for the capacitated fixed charge problems"],
+               "uncap_p_med": [df_uncap_p_med, "Results for the uncapacitated p-median problems"],
+               "cap_p_med": [df_cap_p_med, "Results for the capacitated p-median problems"]}
+
+    return df_dict
+
+
+def add_report_table(df_dict, report_vals, index_vals, col_vals):
+    for ele in df_dict:
+        # If we are using the p-median problem, then we need to include the value of p
+        if re.search('p_med', ele):
+            index_app = ['p']
+        else:
+            index_app = []
+        index_use = index_vals + index_app
+        df_rep = pd.pivot_table(df_dict[ele][0], values=report_vals, index=index_use, columns=col_vals, aggfunc=np.mean,
+                                margins=True, margins_name='Test bed average').round(2)
+        df_rep = df_rep.iloc[:, :-1]
+        df_dict[ele].append(df_rep)
+    return df_dict
+
+
+def create_latex_tables(df_dict, df_position, caption_position, outfile_name):
+    # Function that receives a dictionary of dataframes and captions and prints the corresponding latex tables to
+    # outfile_name
+    with open(outfile_name, "w") as text_file:
+        for ele in df_dict:
+            print(f"{create_latex_table(df_dict[ele][df_position], caption_string=df_dict[ele][caption_position])}",
+                  file=text_file)
+    return None
 
 
 def main():
@@ -50,26 +120,21 @@ def main():
     # Create one concatenated dataframe for all tables read
     df_full = from_csvs_to_dataframe(file_prefix)
 
-    # List of conditional statements
-    uncap_cond = df_full['Cap'] == 0
-    cap_cond = df_full['Cap'] == 1
-    fix_charge_cond = df_full['hybrid'] == 1
-    p_med_cond = df_full['hybrid'] == 0
+    # Split the dataframe into each of the cases
+    df_dict = split_dataframe_by_case(df_full)
 
-    # Uncapacitated dataframes
-    df_uncap_fix_charge = df_full[uncap_cond & fix_charge_cond]
-    df_uncap_p_med = df_full[uncap_cond & p_med_cond]
+    # Define the columns, indices and values to be reported from the dataframes in the dictionaries
+    report_vals = ['CPU_all']
+    index_vals = ['Instance']
+    col_vals = ['Heur_used']
 
-    # Capacitated dataframes
-    df_cap_fix_charge = df_full[cap_cond & fix_charge_cond]
-    df_cap_p_med = df_full[cap_cond & p_med_cond]
+    # Add the dataframes with the reports to the dataframe dictionary
+    df_dict = add_report_table(df_dict, report_vals, index_vals, col_vals)
 
     # Create the Latex tables
-    with open(outfile_name, "w") as text_file:
-        print(f"{create_latex_table(df_uncap_fix_charge, ['Vers', 'instance'])}", file=text_file)
-        print(f"{create_latex_table(df_uncap_p_med, ['Vers', 'instance'])}", file=text_file)
-        print(f"{create_latex_table(df_cap_fix_charge, ['Vers', 'instance'])}", file=text_file)
-        print(f"{create_latex_table(df_cap_p_med, ['Vers', 'instance'])}", file=text_file)
+    create_latex_tables(df_dict, 2, 1, outfile_name)
+
+    print("Finished writing latex tables into " + str(outfile_name))
 
 
 if __name__ == "__main__":
