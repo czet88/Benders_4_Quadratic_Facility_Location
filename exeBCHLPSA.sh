65d7907fd065ecf7554d9b198f4836dde8e5f959
Carlos Zetina <czet_88@hotmail.com>
2021-06-27 16:55:42 -0400

    Setup for quick running

--- exeBCHLPSA.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for i in $(find ${k}*"txt"); do
-	./BCHLPSA ${i} R_${i}_${heur} ${heur} >Logs/O_${i}_${heur} &
-done
---
+++ run_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./Scripts/update.sh
+./BCHLPSA in_u_f.txt R_in_u_f.txt
